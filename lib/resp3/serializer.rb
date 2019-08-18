d3d45ddd3806c73161f5d7966da5998a8936a81f
Jean Boussier <jean.boussier@gmail.com>
2019-08-18 14:29:49 +0200

    Implement integer and float dumping

--- lib/resp3/serializer.rb
+++ lib/resp3/serializer.rb
@@ -5,8 +5,12 @@ module RESP3
     EOL = "\r\n"
     TYPES = {
       String => :dump_string,
+      Integer => :dump_integer,
+      Float => :dump_float,
     }
     private_constant :TYPES
+
+    INTEGER_RANGE = ((2**64 / 2) * -1)..((2**64 / 2) - 1)
 
     class << self
       def dump(payload, buffer=String.new(encoding: Encoding::BINARY, capacity: 1_024))
@@ -14,6 +18,27 @@ module RESP3
       end
 
       private
+
+      def dump_integer(payload, buffer)
+        if INTEGER_RANGE.cover?(payload)
+          buffer << ':' << payload.to_s << EOL
+        else
+          buffer << '(' << payload.to_s << EOL
+        end
+      end
+
+      def dump_float(payload, buffer)
+        buffer << ','
+        case payload
+        when Float::INFINITY
+          buffer << 'inf'
+        when -Float::INFINITY
+          buffer << '-inf'
+        else
+           buffer << payload.to_s
+        end
+        buffer << EOL
+      end
 
       def dump_string(payload, buffer)
         buffer << '$' << payload.bytesize.to_s << EOL << payload << EOL
--- test/serializer_test.rb
+++ test/serializer_test.rb
@@ -6,6 +6,20 @@ class SerializerTest < Minitest::Test
     assert_dumps "Hello World!", "$12\r\nHello World!\r\n"
   end
 
+  def test_dump_integer
+    assert_dumps 42, ":42\r\n"
+  end
+
+  def test_dump_big_integer
+    assert_dumps 1_000_000_000_000_000_000_000, "(1000000000000000000000\r\n"
+  end
+
+  def test_dump_float
+    assert_dumps 42.42, ",42.42\r\n"
+    assert_dumps Float::INFINITY, ",inf\r\n"
+    assert_dumps(-Float::INFINITY, ",-inf\r\n")
+  end
+
   private
 
   def assert_dumps(payload, expected)
